57bb8d291ab6c3fd9c273180e07b69c0554fb599
Anna Astle <ast19004@byui.edu>
2023-01-04 19:33:05 -0800

    Scooter Animation with intersection-observer

--- client/src/components/Pricing/PricingCard.js
+++ client/src/components/Pricing/PricingCard.js
@@ -1,10 +1,25 @@
 import { Box } from "@mui/system";
+import { useEffect, useState } from "react";
+
+import { useInView } from "react-intersection-observer";
 
 import styles from "./PricingCard.module.css";
 
 const PricingCard = (props) => {
+  const [inViewStyle, setInViewStyle] = useState("");
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+  });
+  useEffect(() => {
+    if (inView) {
+      setInViewStyle(styles.slideDownRight);
+    }
+    return () => setInViewStyle("");
+  }, [inView]);
+
   return (
     <Box
+      ref={ref}
       className={styles.priceCard}
       sx={{
         position: "relative",
@@ -15,7 +30,11 @@ const PricingCard = (props) => {
       <span className={`${styles.underlayText} font-accent_secondary`}>
         {props.accentText}
       </span>
-      <img className={styles.scooterImg} src={props.src} alt={props.type} />
+      <img
+        className={`${styles.scooterImg} ${inView && inViewStyle}`}
+        src={props.src}
+        alt={props.type}
+      />
       <Box component="section" className={styles.priceDetails}>
         <h2 className={styles.scooterTitle}>{props.type}</h2>
         <p>Info about the motocycle and how fast it is.</p>
