64421faaed1f5cf88e229d4a6356eecacf643665
Anna Astle <ast19004@byui.edu>
2023-05-10 19:37:12 -0700

    Initial Entry to site fade in

--- src/App.js
+++ src/App.js
@@ -7,17 +7,33 @@ import Welcome from "./components/Welcome";
 import DestinationsContainer from "./components/Destinations/DestinationsContainer";
 import PricingContainer from "./components/Pricing/PricingContainer";
 import RentalCalendarContainer from "./components/Booking/RentalCalendarContainer";
+import { Box } from "@mui/material";
+import Logo from "./components/Header/Logo";
 
 function App() {
   return (
-    <div className="App">
+    <Box className="App" sx={{ position: "relative" }}>
+      <Box
+        className="app-opening"
+        sx={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          zIndex: "2147483647",
+          width: "100%",
+          height: "100%",
+          backgroundColor: "white",
+          display: "flex",
+          justifyContent: "center",
+        }}
+      ></Box>
       <Header />
       <Welcome />
       <DestinationsContainer />
       <PricingContainer />
       <RentalCalendarContainer />
       <ToastContainer />
-    </div>
+    </Box>
   );
 }
 
